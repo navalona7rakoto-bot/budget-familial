262843915fef35f9dcba2d9e4e6ea73fddff099e
Navalona-dev <navalona7rakoto@gmail.com>
2026-03-29 20:55:22 +0300

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/budget-familial/', // Change this to your GitHub repo name
+  base: '/'budget-familial/', // Change this to your GitHub repo name
   build: {
     outDir: 'dist',
   },
